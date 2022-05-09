74e287cb7606828f6465ff6083b62acb2ab485c8
remiScript <92605735+remiScript@users.noreply.github.com>
2022-05-09 12:01:39 -0400

    Convert constructors to factory functions

--- app.js
+++ app.js
@@ -5,55 +5,125 @@ const rainbowContainer = document.getElementById('rainbowContainer')
 const formContainer = document.getElementById('formContainer');
 const formSubmitBtn = document.getElementById('formSubmitBtn');
 
-// Need to create library function - it would create a new
-// library, even though we only need 1. The point is, it would
-// establish what a library is, define all of its functions.
-// If this was a real world example, once we've done this, someone
-// would call these functions and say library.addBook, or library.clearBooks
-// which would be easier to read and make more sense.
-
-let LibraryFactory = function () {
-    // create lib object
-    var library = {};
-
-    // parameters as keys for this object
-    library.titles = [];
-
-    // function to add book
-    library.addBook = function(){
-        return ('book added')
-    };
-    return library; 
-}
-
 
 // library array of all books
-let library = [];
+// let library = [];
 
-// Our factory function that creates book objects
-function createBook(title, author, pages, cover, read) {
-    var book = {};
+// Our library factory that creates library objects
+const libraryFactory = () => {
+    return {
+        collection: [],
+        // logic for adding book object to library collection array
+        addBook(a, b, c, d, e) {
+            this.collection.push(bookFactory(a, b, c, d, e))
+        },
+        // logic for rendering the library array in the DOM
+        renderBook(bTitle, bAuthor, bPages, bCover, bRead) {
+            //store container div as variable
+            const container = document.getElementById('container');
 
-    book.title = title;
-    book.author = author;
-    book.pages = pages;
-    book.cover = cover;
-    book.read = read;
-    return { title, 
-             author, 
-             pages, 
-             cover, 
-             read, 
-             sayTitle: function(){
-                 console.log(`Title: ${book.title}`);
-             }}
+            //create a new div and add a class of card
+            const card = document.createElement('div');
+            card.classList.add('card');
+
+            //append that card div to its parent, the container div
+            container.appendChild(card);
+
+            //create divs, add classes
+            const cardImg = document.createElement("div");
+            const cardInfo = document.createElement("div");
+            cardImg.classList.add('cardImg');
+            cardInfo.classList.add('cardInfo');
+
+            //append cardIMG&cardInfo to card
+            card.appendChild(cardImg);
+            card.appendChild(cardInfo);
+
+            //cardInfo DOM elements:
+            //title
+            const title = document.createElement('p');
+            title.classList.add('title');
+            const titleNode = document.createTextNode(bTitle);
+            title.dataset.title = bTitle;
+            title.appendChild(titleNode);
+            cardInfo.appendChild(title);
+
+            //author
+            const author = document.createElement('p');
+            author.classList.add('author');
+            const authorNode = document.createTextNode(bAuthor);
+            author.appendChild(authorNode);
+            cardInfo.appendChild(author);
+
+            //pages
+            const pages = document.createElement('p');
+            pages.classList.add('pages');
+            const pagesNode = document.createTextNode(`Pages: ${bPages}`);
+            pages.appendChild(pagesNode);
+            cardInfo.appendChild(pages);
+
+            //cover
+            cardImg.style.backgroundImage = 'url(img/coverNotFound.jpg)';
+            if (bCover !== '') {
+                cardImg.style.backgroundImage = `url(${bCover})`;
+            }
+    
+            //haveYouReadThis?
+            const readQuestion = document.createElement('p');
+            readQuestion.classList.add('read');
+            const readQuestionNode = document.createTextNode('haveYouReadThis?');
+            readQuestion.appendChild(readQuestionNode);
+            cardInfo.appendChild(readQuestion);
+
+            // yes/no button
+            const read = document.createElement('button');
+            read.classList.add('readBtn');
+            if (bRead == true) {
+                read.classList.add('readBtnYes');
+                const readNode = document.createTextNode('Yes');
+                read.appendChild(readNode);
+            } else {
+                read.classList.add('readBtnNo');
+                const readNode = document.createTextNode('No');
+                read.appendChild(readNode);
+            }
+            cardInfo.appendChild(read);
+
+            //delete book button
+            const deleteBookBtn = document.createElement('button');
+            const deleteNode = document.createTextNode('delete');
+            deleteBookBtn.appendChild(deleteNode);
+            deleteBookBtn.classList.add('deleteBookBtn');
+            cardInfo.appendChild(deleteBookBtn);
+
+
+            deleteBookBtn.addEventListener('click', function(){
+                const result = lib1.collection.filter(book => book.title !== title.dataset.title);
+                console.log(title.dataset.title);
+                console.log(title)
+                lib1.collection = result;
+                console.table(lib1.collection);
+                title.parentElement.parentElement.remove();
+                return lib1.collection;
+            })
+        }
+    }
 }
 
-// the function that adds a book to the library array
-function addBookToLibrary(a, b, c, d, e) {
-    library.push(createBook(a, b, c, d, e));
-    console.log(a);
-    console.table(library);
+// Our factory function that creates book objects
+const bookFactory = (title, author, pages, cover, read) => {
+    return {
+        title: title,
+        author: author,
+        pages: pages,
+        cover: cover,
+        read: read,
+        exampleFun() {
+            return `string`
+        }
+    }
+    
+
 }
 
 // Remove form and overlay from screen
@@ -74,100 +144,6 @@ function clearBoard() {
     cardContainer.removeChild(card);
     card = cardContainer.lastElementChild;
     }
-}
-
-// logic for rendering the library array in the DOM
-function addBookToDOM(bTitle, bAuthor, bPages, bCover, bRead) {
-
-    //store container div as variable
-    const container = document.getElementById('container');
-
-    //create a new div and add a class of card
-    const card = document.createElement('div');
-    card.classList.add('card');
-
-    //append that card div to its parent, the container div
-    container.appendChild(card);
-
-    //create divs, add classes
-    const cardImg = document.createElement("div");
-    const cardInfo = document.createElement("div");
-    cardImg.classList.add('cardImg');
-    cardInfo.classList.add('cardInfo');
-
-    //append cardIMG&cardInfo to card
-    card.appendChild(cardImg);
-    card.appendChild(cardInfo);
-
-    //cardInfo DOM elements:
-    //title
-    const title = document.createElement('p');
-    title.classList.add('title');
-    const titleNode = document.createTextNode(bTitle);
-    title.dataset.title = bTitle;
-    title.appendChild(titleNode);
-    cardInfo.appendChild(title);
-
-    //author
-    const author = document.createElement('p');
-    author.classList.add('author');
-    const authorNode = document.createTextNode(bAuthor);
-    author.appendChild(authorNode);
-    cardInfo.appendChild(author);
-
-    //pages
-    const pages = document.createElement('p');
-    pages.classList.add('pages');
-    const pagesNode = document.createTextNode(`Pages: ${bPages}`);
-    pages.appendChild(pagesNode);
-    cardInfo.appendChild(pages);
-
-    //cover
-    cardImg.style.backgroundImage = 'url(img/coverNotFound.jpg)';
-    if (bCover !== '') {
-        cardImg.style.backgroundImage = `url(${bCover})`;
-    }
-    
-    
-
-    //haveYouReadThis?
-    const readQuestion = document.createElement('p');
-    readQuestion.classList.add('read');
-    const readQuestionNode = document.createTextNode('haveYouReadThis?');
-    readQuestion.appendChild(readQuestionNode);
-    cardInfo.appendChild(readQuestion);
-
-    // yes/no button
-    const read = document.createElement('button');
-    read.classList.add('readBtn');
-    if (bRead == true) {
-        read.classList.add('readBtnYes');
-        const readNode = document.createTextNode('Yes');
-        read.appendChild(readNode);
-    } else {
-        read.classList.add('readBtnNo');
-        const readNode = document.createTextNode('No');
-        read.appendChild(readNode);
-    }
-    cardInfo.appendChild(read);
-
-    //delete book button
-    const deleteBookBtn = document.createElement('button');
-    const deleteNode = document.createTextNode('delete');
-    deleteBookBtn.appendChild(deleteNode);
-    deleteBookBtn.classList.add('deleteBookBtn');
-    cardInfo.appendChild(deleteBookBtn);
-
-
-    deleteBookBtn.addEventListener('click', function(){
-        const result = library.filter(book => book.title !== title.dataset.title);
-        console.log(title.dataset.title);
-        console.log(title)
-        library = result;
-        console.table(library);
-        title.parentElement.parentElement.remove();
-        return library;
-    })
 }
 
 // logic for the "book++" button, making the form and overlay appear
@@ -212,18 +188,18 @@ formSubmitBtn.addEventListener('click', function(e) {
     // render our new book to the DOM
     // addBookToDOM(bookTitle, bookAuthor, bookPages, bookCover, bookRead);
 
-    addBookToLibrary(bookTitle, bookAuthor, bookPages, bookCover, bookRead);
+    lib1.addBook(bookTitle, bookAuthor, bookPages, bookCover, bookRead);
     clearBoard();
     
     // we need to write a function that loops through each item in the array of objects and
     // runs addBookToDOM with the object values being the arguments
-    library.forEach(book => {
-        // console.log(book);
-        // console.log(book.book);
-        // console.log(book.book.title);
-        addBookToDOM(book.title, book.author, book.pages, book.cover, book.read);
+    lib1.collection.forEach(book => {
+        lib1.renderBook(book.title, book.author, book.pages, book.cover, book.read);
     });
 
     // return the book object so it can be added to the array
     return true;
 })
+
+// create the library
+let lib1 = libraryFactory();
